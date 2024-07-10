bfb631ce75dc980f155917af4c5ffaf703bbdf0f
Ayomide Aregbede <ayo.aregbede@gmail.com>
2024-07-11 00:41:55 +0100

    use io.Copy and setup fly

--- main.go
+++ main.go
@@ -4,13 +4,18 @@ import (
 	"fmt"
 	"io"
 	"net"
+	"os"
 
 	"go.uber.org/zap"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 	// Start listening on a TCP port
-	listener, err := net.Listen("tcp", ":8080")
+	listener, err := net.Listen("tcp", ":"+port)
 	zap.ReplaceGlobals(zap.Must(zap.NewDevelopment()))
 
 	if err != nil {
@@ -34,33 +39,10 @@ func main() {
 }
 
 func handleConnection(conn net.Conn) {
-	fmt.Printf("Serving %s\n", conn.RemoteAddr().String())
+	fmt.Printf("Serving at this address %s\n", conn.RemoteAddr().String())
 	defer conn.Close()
 
-	// Create a reader to read data from the connection
-	// reader := bufio.NewReader(conn)
-	packet := make([]byte, 4096)
-	tmp := make([]byte, 4096)
-
-	for {
-		// Read data from the connection
-		_, err := conn.Read(tmp)
-
-		if err != nil {
-			if err != io.EOF {
-				zap.L().Error("Read error", zap.Error(err))
-			}
-			zap.L().Info("End of file")
-			break
-		}
-
-		zap.L().Info("Data received", zap.String("data", string(tmp[:])))
-
-		// Append the data to the packet
-		packet = append(packet, tmp...)
-	}
-
-	_, err := conn.Write(packet)
+	_, err := io.Copy(conn, conn)
 	if err != nil {
 		zap.L().Error("Error writing to connection", zap.Error(err))
 		return
